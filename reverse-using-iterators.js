e6d8abf77c93879df6b725d0d24e2c1dc5e46a78
Varun Malik <varun1malik@gmail.com>
2018-07-09 16:50:14 +0530

    reverse string using for loop done

--- reverse-using-iterators.js
+++ reverse-using-iterators.js
@@ -1,4 +1,10 @@
 // The intent of this file is to reverse the string using iterators (for, foreach)
-const reverse = () => {};
+const reverse = (s) => {
+  let revstring = '';
+  for (let i = s.length - 1; i >= 0; i -= 1) {
+    revstring += s[i];
+  }
+  return revstring;
+};
 
 module.exports = reverse;
