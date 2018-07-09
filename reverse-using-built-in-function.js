094781281e3d729a208684a2632a876dbc167837
Varun Malik <varun1malik@gmail.com>
2018-07-09 16:58:29 +0530

    return statement added to reverse built in function

--- reverse-using-built-in-function.js
+++ reverse-using-built-in-function.js
@@ -1,4 +1,4 @@
 // The intent of this file is to reverse
 // the string using built in functions.
-const reverse = (s) => { s.split('').reverse().join(''); };
+const reverse = s => s.split('').reverse().join('');
 module.exports = reverse;
